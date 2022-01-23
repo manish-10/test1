035efe60d820bdc865373534780b21ef2d567c97
manish <mks19121999@gmail.com>
2022-01-23 18:14:22 +0530

    second commit
    Signed-off-by: manish <mks19121999@gmail.com>

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,18 @@
 class StudentsController < ApplicationController
   def index
-    @students=Student.all
+    if params[:search].blank?
+      @students=Student.all
+    else
+    @student=params[:search].downcase
+    @students=Student.all.where("lower(Name) LIKE :search", search: "%#{@student}%")
+    end
   end
 
   def new
     @students=Student.new
+  end
+
+  def search 
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'students/index'
   get 'students/new'
+  get 'students/index/:id', to: 'students#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
